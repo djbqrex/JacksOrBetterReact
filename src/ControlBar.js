da43d3428215af835a89ae393a03566dcd136770
Dan Bainey <dan.bainey@gmail.com>
2020-12-16 20:28:40 -0600

    Add controls to controlbar and base logic to change buttons to final draw

--- src/ControlBar.js
+++ src/ControlBar.js
@@ -3,12 +3,41 @@ import './ControlBar.css';
 
 class ControlBar extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {isfirstDraw: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if (e.target.id === "initDraw") {
+      this.props.setfirstDraw();
+    }
+  }
+
   render() {
-    return (
-      <div id="controlBar" tabIndex="0" onKeyDown={this.handleKey}>
-        ControlBar
-      </div>
-    );
+    if (this.props.isfirstDraw === true) {
+      return (
+        <div id="controlBar" >
+          <button id="initDraw" onClick={this.handleClick}>Draw For Jacks [Space]</button>
+        </div>
+      );
+    }
+    else 
+    {
+      return (
+        <div id="controlBar" >
+          <button id="hold1" onClick={this.handleClick}>Hold [z]</button>
+          <button id="hold2" onClick={this.handleClick}>Hold [x]</button>
+          <button id="hold3" onClick={this.handleClick}>Hold [c]</button>
+          <button id="hold4" onClick={this.handleClick}>Hold [v]</button>
+          <button id="hold5" onClick={this.handleClick}>Hold [b]</button>
+          <button id="finalDraw" onClick={this.handleClick}>Final Draw [Space]</button>
+        </div>
+      );
+    }
   }
 };
 
--- src/PlayField.js
+++ src/PlayField.js
@@ -5,15 +5,30 @@ import DeckContainer from './DeckContainer.js'
 import BetBar from './BetBar.js'
 
 class PlayField extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isfirstDraw : true
+    }
+
+    this.setfirstDraw = this.setfirstDraw.bind(this)
+  }
 
   handleKey = event => {
     document.getElementById("playField").textContent = 'KeyCode of Key Press: ' + event.keyCode;
   }
   
+  setfirstDraw() {
+    this.setState({
+      isfirstDraw: false
+    })
+  }
+
   render() {
     return (
       <div id="playField" tabIndex="0" onKeyDown={this.handleKey}>
-        <ControlBar />
+        <ControlBar setfirstDraw={this.setfirstDraw} isfirstDraw={this.state.isfirstDraw}/>
         <DeckContainer />
         <BetBar />
       </div>
